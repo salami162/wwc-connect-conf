22d3f5d6cb4c8baece9223a892600c1c8b291458
Yuanyuan Pao <ypao@lyft.com>
2017-04-18 17:20:58 -0700

    adding script to produce geojson visualization of dataset and centroids

---
+++ connect/utils/geojsonify.py
@@ -0,0 +1,35 @@
+import numpy as np
+from colorutils import Color
+
+
+def visualize_as_geojson(centroids, data, labels):
+    geojson = {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+
+    colors = np.random.randint(255, size=(len(centroids), 3))
+    colors = [Color(tuple(i)).hex for i in colors]
+
+    for i in np.random.choice(len(data), len(data) / 10, replace=False):
+        geojson['features'].append({
+            'type': 'Feature',
+            'properties': {'marker-color': colors[labels[i]]},
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [data[i][1], data[i][0]]
+            }
+        })
+
+    for i in range(len(centroids)):
+        # Add a larger icon for centroids
+        geojson['features'].append({
+            'type': 'Feature',
+            'properties': {'marker-color': colors[i], 'marker-size': 'large', 'marker-symbol': str(i)},
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [centroids[i][1], centroids[i][0]]
+            }
+        })
+
+    return geojson
