dd8bcabc367c090c8777d2fa9548926a24b8e59d
Lita Cho <lcho@lyft.com>
2017-04-23 01:57:10 -0700

    Add KMeans Script

--- manage.py
+++ manage.py
@@ -5,8 +5,11 @@ gevent.monkey.patch_all()
 from flask.ext.script import Manager
 
 from connect import app
+from scripts.kmeans import KMeansCommand
 
 manager = Manager(app)
+manager.add_command('kmeans', KMeansCommand)
 
 if __name__ == '__main__':
     manager.run()
+
