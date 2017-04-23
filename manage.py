33c1201c67a30edca106783587cbaabee0221e9f
salami162 <limin.shen@hotmail.com>
2017-04-22 23:44:57 -0700

    try to add command, but failed

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 import gevent.monkey
 gevent.monkey.patch_all()
 
-
 from flask.ext.script import Manager
 
 from connect import app
+from connect.utils.kmeans import write_data_csv
 
 manager = Manager(app)
+# manager.add_command('kmean', write_data_csv)
 
 if __name__ == '__main__':
     manager.run()
