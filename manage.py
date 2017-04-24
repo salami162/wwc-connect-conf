b95ae3eab1dc61687fca1c5bde914c8aa997c67f
salami162 <limin.shen@hotmail.com>
2017-04-24 12:20:11 -0700

    update manage.py

--- manage.py
+++ manage.py
@@ -4,10 +4,8 @@ gevent.monkey.patch_all()
 from flask.ext.script import Manager
 
 from connect import app
-# from connect.utils.kmeans import write_data_csv
 
 manager = Manager(app)
-# manager.add_command('kmean', write_data_csv)
 
 if __name__ == '__main__':
     manager.run()
