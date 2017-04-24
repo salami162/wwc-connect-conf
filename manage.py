19437187861a6fe2efb00cd7fe8b19d6d716f821
Lita Cho <lcho@lyft.com>
2017-04-24 00:05:00 -0700

    Add Hierarchy Script

--- manage.py
+++ manage.py
@@ -6,9 +6,11 @@ from flask.ext.script import Manager
 
 from connect import app
 from scripts.kmeans import KMeansCommand
+from scripts.hierarchy import HierarchyCommand
 
 manager = Manager(app)
 manager.add_command('kmeans', KMeansCommand)
+manager.add_command('hierarchy', HierarchyCommand)
 
 if __name__ == '__main__':
     manager.run()
